6923672d632886b33dd6ac4eb34e5bb90874f096
Joao Amaral <jamaralm07@gmail.com>
2025-05-09 08:12:54 -0300

    User Interface added

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         ArrayList<Task> taskList = new ArrayList<Task>();
 
-        addTask(taskList);
-        showTaskList(taskList);
-        searchTaskById(taskList);
-        deleteTaskById(taskList);
+        boolean continuar;
+
+        do{
+            System.out.println("Oque deseja fazer?\n 1. Adicionar Tarefa\n2. Remover Tarefa\n3. Listar Tarefas\n4. Buscar Tarefa pelo ID");
+
+            int userInput = sc.nextInt();
+            sc.nextLine();
+
+            switch (userInput){
+                case 1:
+                    addTask(taskList);
+                    break;
+                case 2:
+                    deleteTaskById(taskList);
+                    break;
+                case 3:
+                    searchTaskById(taskList);
+                    break;
+                case 4:
+                    showTaskList(taskList);
+                    break;
+            }
+
+            System.out.println("Deseja Continuar? (S/N)");
+            String userInputToContinue = sc.nextLine();
+
+            continuar = userInputToContinue.equals("S");
+        }while(continuar);
     }
 
     public static void addTask(ArrayList<Task> taskList){
