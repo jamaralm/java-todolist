4b9fb43b56669942208f3d4f0224bbba543b2ea4
João amaral <72447029+jamaralm@users.noreply.github.com>
2025-06-11 22:51:19 -0300

    user class created

---
+++ src/com/classes/User.java
@@ -0,0 +1,22 @@
+package com.classes;
+
+import java.time.LocalDateTime;
+
+public class User {
+
+    private static int idGenerator = 1;
+
+    private int id;
+    private String name;
+    private LocalDateTime created_at;
+
+    public User(String name){
+        this.id = idGenerator++;
+        this.name = name;
+        this.created_at = LocalDateTime.now();
+    }
+
+    public String getName(){
+        return name;
+    }
+}
