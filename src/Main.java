7058d8e4e217d6ab1d8b31f1f195d67e72e2e5b8
João amaral <72447029+jamaralm@users.noreply.github.com>
2025-06-12 23:10:24 -0300

    Main.java Refactored

--- src/Main.java
+++ src/Main.java
@@ -10,31 +10,27 @@ public class Main {
 
         do {
             System.out.println(
-                    "\nBem vindo(a) a central de controle de atividades!\n" +
+                    "\nBem-vindo(a) à Central de Controle de Atividades\n" +
                             "1. Adicionar Tarefa\n" +
-                            "2. Remover Tarefa\n" +
-                            "3. Buscar Tarefa pelo Usuario\n" +
-                            "4. Listar Tarefas\n" +
-                            "5. Editar Tarefa\n" +
-                            "6. Começar Tarefa\n" +
-                            "7. Concluir Tarefa\n\n" +
-                            "O que deseja fazer? "
+                            "2. Remover Tarefa por Título\n" +
+                            "3. Buscar Tarefa por Título\n" +
+                            "4. Buscar Tarefa por Usuário\n" +
+                            "5. Listar Tarefas\n" +
+                            "6. Editar Tarefa por Título\n" +
+                            "7. Começar Tarefa\n" +
+                            "8. Concluir Tarefa\n" +
+                            "0. Sair\n" +
+                            "Escolha uma opção: "
             );
 
-            while(!sc.hasNextInt()){
+            while (!sc.hasNextInt()) {
                 System.out.println("Insira um número válido!");
                 sc.nextLine();
             }
-            int userInput = sc.nextInt();
+            int opcao = sc.nextInt();
             sc.nextLine();
 
-            while(userInput < 0 || userInput > 7){
-                System.out.println("Insira um número válido!");
-                userInput = sc.nextInt();
-                sc.nextLine();
-            }
-
-            switch (userInput){
+            switch (opcao) {
                 case 1:
                     manager.addTask(sc);
                     break;
@@ -45,28 +41,35 @@ public class Main {
                     manager.searchTaskById(sc);
                     break;
                 case 4:
-                    manager.showTaskList();
+                    manager.searchUserTasks(sc);
                     break;
                 case 5:
+                    manager.showTaskList();
+                    break;
+                case 6:
                     manager.editTaskById(sc);
                     break;
-                case 6, 7:
+                case 7, 8:
                     manager.markTaskAsDone(sc);
                     break;
+                case 0:
+                    continuar = false;
+                    System.out.println("Encerrando o sistema...");
+                    return;
                 default:
                     System.out.println("Opção inválida.");
             }
 
-            System.out.println("Deseja Continuar? (S/N)");
-            String userInputToContinue = sc.nextLine();
+            System.out.println("Deseja continuar? (S/N)");
+            String resposta = sc.nextLine();
 
-            while (!userInputToContinue.equalsIgnoreCase("S") && !userInputToContinue.equalsIgnoreCase("N")) {
-                System.out.println("Insira um valor válido! (S/N)");
-                userInputToContinue = sc.nextLine();
+            while (!resposta.equalsIgnoreCase("S") && !resposta.equalsIgnoreCase("N")) {
+                System.out.println("Insira um valor válido (S/N):");
+                resposta = sc.nextLine();
             }
 
-            continuar = userInputToContinue.equalsIgnoreCase("S");
+            continuar = resposta.equalsIgnoreCase("S");
 
-        } while(continuar);
+        } while (continuar);
     }
 }
